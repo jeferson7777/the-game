41654c521e4e290e5d88230ff6cb0a559d0b0a04
Jefersoon <jeffchc11@gmail.com>
2020-09-30 16:14:45 +0200

    Actualizado1.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')//
   const width = 18///
   const squares = []
-  // const mySound = document.querySelector('audio')
+
   let snake = [3, 2, 1, 0] // 3 cabeza  2.1 cuerpo    0 cola  //
   //let snake2 = //podriamos colocar otra serpiente
-  let direction = 'right'
+  let direction = 0
   const scoreDisplay = document.querySelector('.score')
   let score = 0 //
-  let speedSnake = 300  ///milisegundo
+  let speedSnake = 350  ///milisegundo
   const resetBtn = document.querySelector('button')
   document.querySelector('.dead')   //podria añadir una constante aqui¿?
   let timer
-  // document.getElementById('themeL').loop = true ///es posible ELIMINACION pq es AUDIO
+
 
   // create a grid
   for (let i = 0; i < width * width; i++) {
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     squares.push(square)
     grid.appendChild(square)
   }
-  //COPIANDOO NOKIA PARA START Y RESTART EL JUEGO
+  //PARA START Y RESTART EL JUEGO
   function startGame() {
     snake.forEach(index => squares[index].classList.remove('snake'))
   }
@@ -30,6 +30,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function drawSnake() {
     snake.forEach(index => squares[index].classList.add('snake')) //hace refencia a snake y lo esta creando dentro de square  y en el index
   }
+
+
+  /////////////////////////////
+
 
 })
 
