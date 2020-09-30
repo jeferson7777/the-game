cdf14ba7c18e8c3d90932d32b0ff8bf8f19a2e06
Jefersoon <jeffchc11@gmail.com>
2020-09-30 15:47:19 +0200

    primer guardado SNAKE 1.0

---
+++ styles/texto.js
@@ -0,0 +1,25 @@
+//seudocodigo
+//1.programaticamente crear celdas en HTML y//////////////////////////////////////////////////////////////
+//pegarlas dentro del elemento con la clase.grid
+
+
+//
+//funcion forloop
+// funcion nativa JS for loop
+//la usamos para contar de 1 a 100 o de 0 a 99
+//tiene tres argumentos separados por punt y coma: la primera se llame le iterador es decir el pointer = puntero.
+//el segundo argumento nos dice  la condicion para que el loop se pare:
+//  es decir cuando Index que es el que va contando desde cero hasta..sea menos qu el numero de celdas
+// el tercer argumente es q pasa en cada bucle 
+
+
+//hago que el documento me cree un div algo no semantico, y eso lo coloco en uuna constante.
+
+//el texto que quiero integrar sea numeros, el index.y que lo pegue dentro de grid, pero hay que seleccionar un elemento con clase.grid en HTML
+  // grid tenga como hijo cada una de esas celulas y div qu acabo de crear
+  //  y con todo esto cree programaticamente DIV dentro de la seccion GRID
+
+
+
+//es lo mismo que const key = event.key;  // indica laas direcciones 
+  // console.log(key);        // esto una forma de escribir una costante llamada key que es  una propiedad del OBjeto que estas pasando, a esta sintaxis se le llama DESESTRUCTURACION
